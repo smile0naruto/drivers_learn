73df54811dca717dc2b7dde2d5df9ade2b9fa0e7
smile0naruto <657049815@qq.com>
2016-09-09 15:08:37 +0800

    add kk_log

--- ko_test/ko_test.c
+++ ko_test/ko_test.c
@@ -9,6 +9,26 @@
 #include <linux/of_irq.h>
 #include <linux/platform_device.h>
 
+//#define Enable_KKDRV_LOG 3
+#define KK_TEST_CRTI 1
+#define KK_TEST_FULL 2
+
+static unsigned int kk_log_state_flag = 3;
+
+#define kk_log(num, fmt, args...) \
+do {\
+	if (kk_log_state_flag >= (int)num) \
+		switch (num) {\
+		case KK_TEST_CRTI:\
+				 pr_err(fmt, ##args); \
+				 break; \
+		/*fall-through*/\
+		default: \
+				pr_debug(fmt, ##args); \
+			  break; \
+		} \
+} while (0)
+
 //#define DEVICE_CREAT
 #define USE_LEDS_C
 #define SIZE 10
@@ -20,7 +40,7 @@ module_param(string_param, charp, 0644);
 module_param_array(array_param, int, &num, 0644);
 
 static char kk_test_state1;
-static struct device *kk_test_dev;
+//static struct device *kk_test_dev;
 
 struct device_node *kk_test_node;
 static unsigned int kk_test_en;
@@ -69,6 +89,7 @@ static struct platform_driver kk_test_drv = {
                    },
 };
 
+//kk_test_state
 #ifdef DEVICE_CREAT
 static ssize_t kk_test_show_state(struct device *dev, struct device_attribute *attr, char *buf)
 #else
@@ -87,10 +108,10 @@ static ssize_t kk_test_store_state(struct device_driver *driver, const char *buf
 #endif
 {
 		int ret;
-		int res;
+//		int res;
 		ret = sscanf(buf, "%d", &kk_test_state1);
 		if (ret != 1) {
-				printk("kpd call state: Invalid values\n");
+				kk_log(KK_TEST_CRTI,"kpd call state: Invalid values\n");
 				return -EINVAL;
 		}
 		if (kk_test_state1 == 1){
@@ -108,18 +129,54 @@ static ssize_t kk_test_store_state(struct device_driver *driver, const char *buf
 				pinctrl_select_state(kk_test_pin,pinctrl_kk_test_set_low);
 				//res = snprintf(buf, PAGE_SIZE, "enter error!!!!!%d\n", kk_test_state1);
 		}	
-			return kk_test_state1;
+			return count;
 				
 }
+
+//kk_log_state
+#ifdef DEVICE_CREAT
+static ssize_t kk_log_show_state(struct device *dev, struct device_attribute *attr, char *buf)
+#else
+static ssize_t kk_log_show_state(struct device_driver *driver, char *buf)
+#endif
+{
+	ssize_t res;
+
+	kk_log(KK_TEST_FULL,"=====%s====%d==\n\n",__func__,kk_log_state_flag);
+	res = snprintf(buf, PAGE_SIZE, "%d\n", kk_log_state_flag);
+	return res;
+}
+#ifdef DEVICE_CREAT
+static ssize_t kk_log_store_state(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
+#else
+static ssize_t kk_log_store_state(struct device_driver *driver, const char *buf,size_t count)
+#endif
+{
+		int ret;
+//		int res;
+		ret = sscanf(buf, "%d", &kk_log_state_flag);
+		if (ret == -1) {
+				kk_log(KK_TEST_CRTI,"kk_test state: Invalid values\n");
+				return -EINVAL;
+		}
+		kk_log(KK_TEST_CRTI, "the kk_log_state_flag is %d \n\n",kk_log_state_flag);
+		
+		return count;
+				
+}
+
 #ifdef DEVICE_CREAT
 static DEVICE_ATTR(kk_test_state, 0644, kk_test_show_state, kk_test_store_state);
+static DEVICE_ATTR(kk_log_state,  0644, kk_log_show_state, kk_log_store_state); 
 #else
-static DRIVER_ATTR(kk_test_state, 0644, kk_test_show_state, kk_test_store_state); 
+static DRIVER_ATTR(kk_test_state, 0644, kk_test_show_state, kk_test_store_state);
+static DRIVER_ATTR(kk_log_state,  0644, kk_log_show_state, kk_log_store_state); 
 #endif
 
 #ifndef DEVICE_CREAT
 static struct driver_attribute *kk_test_attr_list[] = {
-	&driver_attr_kk_test_state,        
+	&driver_attr_kk_test_state,
+	&driver_attr_kk_log_state,        
 };
 
 static int kk_test_create_attr(struct device_driver *driver)
@@ -128,14 +185,13 @@ static int kk_test_create_attr(struct device_driver *driver)
 	int num = (int)(sizeof(kk_test_attr_list) / sizeof(kk_test_attr_list[0]));
 
 	if (driver == NULL)
-			printk("there is not real driver,just sys attr???!!!\n\n");//		return -EINVAL;
+			kk_log(KK_TEST_CRTI,"there is not real driver,just sys attr???!!!\n\n");//		return -EINVAL;
 
-	printk("%s,num is %d \n\n",__func__,num);
+	kk_log(KK_TEST_FULL,"%s,num is %d \n\n",__func__,num);
 	for (idx = 0; idx < num; idx++) {
 		err = driver_create_file(driver, kk_test_attr_list[idx]);
-					printk("111driver_create_file (%s) = %d\n", kk_test_attr_list[idx]->attr.name, err);
 		if (err) {
-			printk("222driver_create_file (%s) = %d\n", kk_test_attr_list[idx]->attr.name, err);
+			kk_log(KK_TEST_CRTI,"222driver_create_file (%s) = %d\n", kk_test_attr_list[idx]->attr.name, err);
 			break;
 		}
 	}
@@ -162,65 +218,65 @@ static int kk_test_get_gpio_info(struct platform_device *pdev)
 		
 		kk_test_node = of_find_compatible_node(NULL, NULL, "kk,kk_test");		
 		if (kk_test_node == NULL) {
-				printk("KK_TEST - get kk_test node failed\n");
+				kk_log(KK_TEST_CRTI,"KK_TEST - get kk_test node failed\n");
 		} else {
 				if (of_property_read_u32_index(kk_test_node, "kk_test_en", 0, &kk_test_en)) {
 						kk_test_en_if_config = 0;
-						printk("get dtsi kk_test_en fail\n");
+						kk_log(KK_TEST_CRTI,"get dtsi kk_test_en fail\n");
 				}
 				if (of_property_read_u32_index(kk_test_node, "kk_test_en", 1, &kk_test_en_mode))
-						printk("get dtsi kk_test_en_mode fail\n");
+						kk_log(KK_TEST_CRTI,"get dtsi kk_test_en_mode fail\n");
 
 				if (of_property_read_u32_index(kk_test_node, "kk_test_el", 0, &kk_test_el)) {
 						kk_test_el_if_config = 0;
-						printk("get dtsi kk_test_el fail\n");
+						kk_log(KK_TEST_CRTI,"get dtsi kk_test_el fail\n");
 				}
 				if (of_property_read_u32_index(kk_test_node, "kk_test_el", 1, &kk_test_el_mode))
-						printk("get dtsi kk_test_el_mode fail\n");
+						kk_log(KK_TEST_CRTI,"get dtsi kk_test_el_mode fail\n");
 				
-				printk("====kk_test===check en is %d ,en mode is %d,el pin is %d,el mode is %d== \n",kk_test_en,kk_test_en_mode,kk_test_el,kk_test_el_mode);												
+				kk_log(KK_TEST_CRTI,"====kk_test===check en is %d ,en mode is %d,el pin is %d,el mode is %d== \n",kk_test_en,kk_test_en_mode,kk_test_el,kk_test_el_mode);												
 		}		
 						
 		kk_test_pin = devm_pinctrl_get(&pdev->dev);
 		if(IS_ERR(kk_test_pin)){
 				ret = PTR_ERR(kk_test_pin);
-				printk("can't find kk_test pinctrl!\n");
+				kk_log(KK_TEST_CRTI,"can't find kk_test pinctrl!\n");
 		}
 		
 		pinctrl_kk_test_def = pinctrl_lookup_state(kk_test_pin, "kk_test_gpio_default");
 		if (IS_ERR(pinctrl_kk_test_def)) {
 				ret = PTR_ERR(pinctrl_kk_test_def);
-				printk("Cannot find pinctrl_kk_test_def pinctrl!\n");
+				kk_log(KK_TEST_CRTI,"Cannot find pinctrl_kk_test_def pinctrl!\n");
 		}		
 		
 		pinctrl_kk_test_def_low = pinctrl_lookup_state(kk_test_pin, "kk_test_gpio_default_low");
 		if (IS_ERR(pinctrl_kk_test_def_low)) {
 				ret = PTR_ERR(pinctrl_kk_test_def_low);
-				printk("Cannot find pinctrl_kk_test_def_low pinctrl!\n");
+				kk_log(KK_TEST_CRTI,"Cannot find pinctrl_kk_test_def_low pinctrl!\n");
 		}
 		
 		pinctrl_kk_test_def_high = pinctrl_lookup_state(kk_test_pin, "kk_test_gpio_default_high");
 		if (IS_ERR(pinctrl_kk_test_def_high)) {
 				ret = PTR_ERR(pinctrl_kk_test_def_high);
-				printk("Cannot find pinctrl_kk_test_def_high pinctrl!\n");
+				kk_log(KK_TEST_CRTI,"Cannot find pinctrl_kk_test_def_high pinctrl!\n");
 		}
 
 		pinctrl_kk_test_set = pinctrl_lookup_state(kk_test_pin, "kk_test_gpio_set");
 		if (IS_ERR(pinctrl_kk_test_set)) {
 				ret = PTR_ERR(pinctrl_kk_test_set);
-				printk("Cannot find pinctrl_kk_test_set pinctrl!\n");
+				kk_log(KK_TEST_CRTI,"Cannot find pinctrl_kk_test_set pinctrl!\n");
 		}	
 		
 		pinctrl_kk_test_set_low = pinctrl_lookup_state(kk_test_pin, "kk_test_gpio_set_low");
 		if (IS_ERR(pinctrl_kk_test_set_low)) {
 				ret = PTR_ERR(pinctrl_kk_test_set_low);
-				printk("Cannot find pinctrl_kk_test_set_low pinctrl!\n");
+				kk_log(KK_TEST_CRTI,"Cannot find pinctrl_kk_test_set_low pinctrl!\n");
 		}
 		
 		pinctrl_kk_test_set_high = pinctrl_lookup_state(kk_test_pin, "kk_test_gpio_set_high");
 		if (IS_ERR(pinctrl_kk_test_set_high)) {
 				ret = PTR_ERR(pinctrl_kk_test_set_high);
-				printk("Cannot find pinctrl_kk_test_set_high pinctrl!\n");
+				kk_log(KK_TEST_CRTI,"Cannot find pinctrl_kk_test_set_high pinctrl!\n");
 		}		
 	
 		return 0;
@@ -247,16 +303,16 @@ static int kk_test_check_bl_status(const char *filename)
 	
 	bl = filp_open(filename,O_RDONLY,0);
 	if (IS_ERR(bl)) {
-			printk("===kk_test open %s failed!!!\n",filename/*,strerror(errno)*/);
+			kk_log(KK_TEST_CRTI,"===kk_test open %s failed!!!\n",filename/*,strerror(errno)*/);
 			return -1;
 	} else {
 	//	ret = bl->f_op->llseek(bl, 0, SEEK_SET);
 		len = bl->f_op->read(bl,buf,1,bl->f_op);
 		if (len < 0){
-			printk("===kk_test read %s buf failed!!\n",filename);
+			kk_log(KK_TEST_CRTI,"===kk_test read %s buf failed!!\n",filename);
 			return -1;
 		}
-		printk("===kk_test check the buf[0] is %d ==len is %d =\n",buf[0],len);
+		kk_log(KK_TEST_CRTI,"===kk_test check the buf[0] is %d ==len is %d =\n",buf[0],len);
 		if ( buf[0] == '0')
 			kk_test_bl_status = 0;
 		else if ( buf[0] > '0')
@@ -273,7 +329,7 @@ static void kk_test_hall_handler(void)
 {
 		int ret;
 		int bl_status;
-		printk("===kk_test===%s ==\n",__func__);
+		kk_log(KK_TEST_CRTI,"===kk_test===%s ==\n",__func__);
 	
 		ret = gpio_get_value(kk_test_en);
 #ifdef USE_LEDS_C
@@ -281,7 +337,7 @@ static void kk_test_hall_handler(void)
 #else		
 		bl_status = kk_test_check_bl_status(bl_path);
 #endif
-		printk ("====kk_test===check io is %d =%d===\n",ret,bl_status);
+		kk_log(KK_TEST_CRTI,"====kk_test===check io is %d =%d===\n",ret,bl_status);
 				
 		if ( bl_status == 1 ){
 			if (ret == 1){
@@ -292,11 +348,11 @@ static void kk_test_hall_handler(void)
     			input_sync(kk_test_input_dev);
     			kk_test_flag = 1;     
   		} else if (ret == 0){
-  				printk("===konka===1===\n");
+  				kk_log(KK_TEST_CRTI,"===konka===1===\n");
   		}
   	}else if (bl_status == 0){
   		if ( ret == 1)
-  				printk("===konka===2===\n");
+  				kk_log(KK_TEST_CRTI,"===konka===2===\n");
   		else if (ret == 0){
   	 			input_report_key(kk_test_input_dev, KEY_POWER, 1);
     			input_sync(kk_test_input_dev);       
@@ -332,24 +388,24 @@ static void kk_test_init_irq(void)
 		u32 ints[2] = { 0, 0 };
 		struct device_node *node = NULL;
 		
-		printk("===kk_test==%s==%d==\n",__func__,kk_test_en);
+		kk_log(KK_TEST_CRTI,"===kk_test==%s==%d==\n",__func__,kk_test_en);
 
 	  kk_test_hall_thread = kthread_run(kk_test_hall_event_handler, 0, "kk_test_hall");
     if (IS_ERR(kk_test_hall_thread))
     { 
-        printk("%s() creat thread fail!!\n",__func__);
+        kk_log(KK_TEST_CRTI,"%s() creat thread fail!!\n",__func__);
     }		
     	
 		    /*gpio*/
 		ret = gpio_request(kk_test_en, "gpio_hall_eint");
 		if (ret < 0)
-				printk("mt_hall_init gpio request error!\n");
+				kk_log(KK_TEST_CRTI,"mt_hall_init gpio request error!\n");
 		ret = gpio_direction_input(kk_test_en);
 		if (ret < 0)
-				printk("mt_hall_init gpio direction error!\n");
+				kk_log(KK_TEST_CRTI,"mt_hall_init gpio direction error!\n");
 		ret = gpio_to_irq(kk_test_en);
 		if (ret < 0)
-				printk("mt_hall_init gpio to irq error!\n");
+				kk_log(KK_TEST_CRTI,"mt_hall_init gpio to irq error!\n");
 
 #if 1				
 		node = of_find_matching_node(node, kk_test_of_match);
@@ -361,13 +417,13 @@ static void kk_test_init_irq(void)
 				ret =request_irq(kk_test_hall_irq, (irq_handler_t)kk_test_hall_eint_handler, IRQF_TRIGGER_MASK,"kk_test-eint", NULL);
 				if (ret > 0) {
 						ret = -1;
-						printk("kk_test_hall request_irq IRQ LINE NOT AVAILABLE!.\n");
+						kk_log(KK_TEST_CRTI,"kk_test_hall request_irq IRQ LINE NOT AVAILABLE!.\n");
 				}
 		} else {
-				printk("kk_test_hall request_irq can not find touch eint device node!.\n");
+				kk_log(KK_TEST_CRTI,"kk_test_hall request_irq can not find touch eint device node!.\n");
 				ret = -1;
 		}
-		printk("[%s]irq:%d, debounce:%d-%d:\n", __func__, kk_test_hall_irq, ints[0], ints[1]);
+		kk_log(KK_TEST_CRTI,"[%s]irq:%d, debounce:%d-%d:\n", __func__, kk_test_hall_irq, ints[0], ints[1]);
 #endif	
 }
 
@@ -375,9 +431,9 @@ static void kk_test_init_irq(void)
 static int kk_test_probe(struct platform_device *pdev)
 {
 	
-		int i,err = 0;
+		int err = 0;
 		
-		printk("\n\n\n\n====welcome to kk world!!!\n\n\n\n");
+		kk_log(KK_TEST_CRTI,"\n\n\n\n====welcome to kk world!!!\n\n\n\n");
 		
 		kk_test_get_gpio_info(pdev);	
 		kk_test_input_dev = input_allocate_device();  //  分配输入设备结构体
@@ -394,7 +450,7 @@ static int kk_test_probe(struct platform_device *pdev)
 		__set_bit(KEY_POWER, kk_test_input_dev->keybit);  //注册电power按键	        
 		err = input_register_device(kk_test_input_dev); //注册一个输入设备
     if (err){
-        	printk("%s regist input device failed!!!\n",__func__);
+        	kk_log(KK_TEST_CRTI,"%s regist input device failed!!!\n",__func__);
           input_free_device(kk_test_input_dev);
           return err;
       }	
@@ -403,11 +459,11 @@ static int kk_test_probe(struct platform_device *pdev)
 #ifdef DEVICE_CREAT
 		err = device_create_file(&pdev->dev, &dev_attr_kk_test_state);		//dev_attr driver_attr_kk_test_state
 		if (err < 0 )
-				printk("Failed to create device file(%s)!\n", dev_attr_kk_test_state.attr.name);
+				kk_log(KK_TEST_CRTI,"Failed to create device file(%s)!\n", dev_attr_kk_test_state.attr.name);
 #else 
 		err = kk_test_create_attr(&kk_test_drv.driver);//err = driver_create_file(&kk_test.driver, &driver_attr_kk_test_state);
 		if (err){
-				printk("Failed to create device file(%s)!\n", driver_attr_kk_test_state.attr.name);
+				kk_log(KK_TEST_CRTI,"Failed to create device file(%s)!\n", driver_attr_kk_test_state.attr.name);
 				kk_test_delete_attr(&kk_test_drv.driver);
 				return err;
 			}
@@ -425,11 +481,11 @@ static int kk_test_remove(struct platform_device *pdev)
 static int kk_test_init(void)
 {
 	int ret;
-	printk("====enter ko world==\n");
+	kk_log(KK_TEST_CRTI,"====enter ko world==\n");
 	
   ret = platform_driver_register(&kk_test_drv);
   if (ret) {
-  		printk("register driver failed (%d)\n", ret);
+  		kk_log(KK_TEST_CRTI,"register driver failed (%d)\n", ret);
       return ret;
   }
 	
@@ -438,7 +494,9 @@ static int kk_test_init(void)
 
 static void  kk_test_exit(void)
 {
-	printk("=====goodbye ====\n");
+	kk_log(KK_TEST_CRTI,"===%s==goodbye ====\n",__func__);
+	platform_driver_unregister(&kk_test_drv);
+	kk_log(KK_TEST_CRTI,"===%s==exit ====\n",__func__);
 }
 
 
